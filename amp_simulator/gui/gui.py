ee7d56f2d66d63f98a150dd5fa05d40f64ba035d
Tanner <tanner.niebling@gmail.com>
2026-06-30 16:04:02 -0500

    Added Tabs to the UI

--- amp_simulator/gui/gui.py
+++ amp_simulator/gui/gui.py
@@ -1,53 +1,39 @@
 import dearpygui.dearpygui as dpg
 
+from .windows.main_amp import create_tab as create_main_amp
+from .windows.pedal_board import create_tab as create_pedal_board
+from .windows.waveform_viewer import create_tab as create_waveform_viewer
+
+
 def start_gui(params):
+
     dpg.create_context()
-    dpg.create_viewport(title="Amp Simulator", width=1000, height=500)
 
-    with dpg.window(label="Amp Control Panel", width=980, height=460):
+    dpg.create_viewport(
+        title="Amp Simulator",
+        width=1000,
+        height=600
+    )
 
-        def update_gain(sender, app_data):
-            params.gain = app_data
+    with dpg.window(
+        label="Amp Simulator",
+        width=985,
+        height=560,
+        no_close=True,
+        no_resize=True,
+        no_move=True,
+    ):
 
-        def update_drive(sender, app_data):
-            params.drive = app_data
+        with dpg.tab_bar():
 
-        def update_volume(sender, app_data):
-            params.volume = app_data
+            with dpg.tab(label="Main Amp"):
+                create_main_amp(params)
 
-        dpg.add_text("Input Stage")
+            with dpg.tab(label="Pedal Board"):
+                create_pedal_board(params)
 
-        dpg.add_knob_float(
-            label="Gain",
-            default_value=params.gain,
-            min_value=0.0,
-            max_value=10.0,
-            callback=update_gain
-        )
-
-        dpg.add_spacing(count=2)
-
-        dpg.add_text("Amp Drive Stage")
-
-        dpg.add_knob_float(
-            label="Drive",
-            default_value=params.drive,
-            min_value=0.0,
-            max_value=10.0,
-            callback=update_drive
-        )
-
-        dpg.add_spacing(count=2)
-
-        dpg.add_text("Output Stage")
-
-        dpg.add_knob_float(
-            label="Volume",
-            default_value=params.volume,
-            min_value=0.0,
-            max_value=10.0,
-            callback=update_volume
-        )
+            with dpg.tab(label="Waveform Viewer"):
+                create_waveform_viewer(params)
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
