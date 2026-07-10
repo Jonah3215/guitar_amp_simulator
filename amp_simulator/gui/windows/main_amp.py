82d1a34f7d0b91f8b086e4021082a83a2744127f
Tanner <tanner.niebling@gmail.com>
2026-07-10 15:18:59 -0500

    Updated UI for more knobs and checklist

--- amp_simulator/gui/windows/main_amp.py
+++ amp_simulator/gui/windows/main_amp.py
@@ -1,6 +1,8 @@
 import dearpygui.dearpygui as dpg
 
-def create_tab(params):
+def create_tab(app):
+    cabinet = app.cabinet
+    params = app.params
 
     def update_gain(sender, app_data):
         params.gain = app_data
@@ -8,8 +10,24 @@ def create_tab(params):
     def update_drive(sender, app_data):
         params.drive = app_data
 
+    def update_bass(sender, app_data):
+        params.bass = app_data
+
+    def update_mid(sender, app_data):
+        params.mid = app_data
+
+    def update_treble(sender, app_data):
+        params.treble = app_data
+
     def update_volume(sender, app_data):
         params.volume = app_data
+
+    def update_ir(sender, app_data):
+        cabinet.load_ir(app.ir_list[app.ir_list.index(app_data)])
+        
+
+    def toggle_ir(sender, app_data):
+        params.ir_enabled = app_data
 
     dpg.add_text("Input Stage")
 
@@ -46,3 +64,46 @@ def create_tab(params):
     )
 
     dpg.add_spacing(count=2)
+
+    dpg.add_knob_float(
+        label="Bass",
+        default_value=params.bass,
+        min_value=0.0,
+        max_value=10.0,
+        callback=update_bass
+    )
+
+    dpg.add_spacing(count=2)
+
+    dpg.add_knob_float(
+        label="Mids",
+        default_value=params.mid,
+        min_value=0.0,
+        max_value=10.0,
+        callback=update_mid
+    )
+
+    dpg.add_spacing(count=2)
+
+    dpg.add_knob_float(
+        label="Treble",
+        default_value=params.treble,
+        min_value=0.0,
+        max_value=10.0,
+        callback=update_treble
+    )
+
+    dpg.add_spacing(count=2)
+
+    dpg.add_combo(
+        label="Impulse Response",
+        items = app.ir_list,
+        default_value=app.ir_list[0],
+        callback=update_ir
+    )
+
+    dpg.add_checkbox(
+        label="Enable IR",
+        default_value=params.ir_enabled,
+        callback=toggle_ir
+)
